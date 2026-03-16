353aaeda1e9d0b448ed4f6668d8084e2483deab0
Rodrigo Figueroa <rodrigofigueroacamacho@gmail.com>
2026-03-15 21:16:12 -0400

    Añadido metodo coseno y respetado codigo de Jorge - Rodrigo Figueroa

--- Aritmetica.java
+++ Aritmetica.java
@@ -12,13 +12,20 @@ public class Aritmetica {
         return (10 * peso) + (6.25 * altura) - (5 * edad) + s;
     }
 
+    public static double cosenoGrados_FigueroaRodrigo(double grados){
+        double radianes = Math.toRadians(grados);
+        return Math.cos(radianes);
+    }
     public static void main(String[] args) {
-        
+        e
         // === Jorge Paniagua ===
         // 1. Llamada para un hombre (s = 5)
         // Datos: 80kg, 180cm, 30 años
         double tmbHombre = Aritmetica.calcularTMB_Paniagua_Jorge(80.0, 180.0, 30, 5);
         System.out.println("TMB para hombre: " + tmbHombre + " calorías/día");
+        
+        double angulo = 60.0;
+        System.out.println("El coseno de " + angulo + " grado es: " +  cosenoGrados_FigueroaRodrigo(angulo));
     }
 
 }
